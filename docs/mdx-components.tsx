de2898dc494ce95f2005acb557c93ebc9418244d
Erwin Kuhn <erwin.kuhn@protonmail.com>
2026-01-21 01:00:10 +0100

    remove fumadocs-python

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,11 +1,9 @@
-import * as Python from "fumadocs-python/components";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
-		...Python,
 		...components,
 	};
 }
